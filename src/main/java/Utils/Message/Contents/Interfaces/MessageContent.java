f7bd2426cd0cf272f3e362e5e7bd62ae2edb4e0b
[ComaN10] <[carlosmanuel2003@gmail.com]>
2024-05-10 23:27:55 +0100

    Doc: Documentation Added to the Message Content Interfaces

--- src/main/java/Utils/Message/Contents/Interfaces/MessageContent.java
+++ src/main/java/Utils/Message/Contents/Interfaces/MessageContent.java
@@ -5,13 +5,36 @@ import Utils.Message.EnumTypes.ContentSubtype;
 
 import java.io.Serializable;
 
-public interface MessageContent extends Serializable
-{
-    byte[] getByteMessage ();
+/**
+ * The MessageContent interface represents the content of a message in a messaging system.
+ */
+public interface MessageContent extends Serializable {
 
-    String getStringMessage ();
+    /**
+     * Returns the byte representation of the message content.
+     *
+     * @return byte array representing the message content.
+     */
+    byte[] getByteMessage();
 
+    /**
+     * Returns the string representation of the message content.
+     *
+     * @return string representing the message content.
+     */
+    String getStringMessage();
+
+    /**
+     * Returns the type of the message content.
+     *
+     * @return ContentTypes representing the type of the message content.
+     */
     ContentTypes getType();
-    ContentSubtype getSubType();
 
+    /**
+     * Returns the subtype of the message content.
+     *
+     * @return ContentSubtype representing the subtype of the message content.
+     */
+    ContentSubtype getSubType();
 }
--- src/main/java/Utils/Message/Contents/Interfaces/MessageContentIntegrityHash.java
+++ src/main/java/Utils/Message/Contents/Interfaces/MessageContentIntegrityHash.java
@@ -1,7 +1,22 @@
 package Utils.Message.Contents.Interfaces;
 
-public interface MessageContentIntegrityHash extends MessageContent
-{
+/**
+ * The MessageContentIntegrityHash interface represents message content that includes integrity hash information.
+ * This interface extends the MessageContent interface.
+ */
+public interface MessageContentIntegrityHash extends MessageContent {
+
+    /**
+     * Returns the digest (hash) associated with the message content.
+     *
+     * @return byte array representing the digest (hash) of the message content.
+     */
     byte[] getDigest();
+
+    /**
+     * Checks if the message content has a valid digest (hash).
+     *
+     * @return true if the message content's digest (hash) is valid, false otherwise.
+     */
     boolean hasValidDigest();
 }
